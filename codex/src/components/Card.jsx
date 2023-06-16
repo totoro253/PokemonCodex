859cf67b17390be5a92a58a4f41163703a7382df
Totoro253 <61892539+totoro253@users.noreply.github.com>
2023-06-16 13:52:47 +0200

    created card component

---
+++ codex/src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Card = () =>  {
+    return(
+        <div className="card">
+            <h2>1</h2>
+            <img  src=""  alt=""/>
+        </div>
+    )
+}
