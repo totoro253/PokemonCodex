8cbf9270985cb72a7c085999791dfc9803d15603
Totoro253 <61892539+totoro253@users.noreply.github.com>
2023-06-16 20:54:25 +0200

    created loading functions

--- codex/src/components/Main.jsx
+++ codex/src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import  Card from './Card';
 import PokeInfo from "./Pokemoninfo";
 import axios from  'axios'
@@ -6,6 +6,21 @@ import axios from  'axios'
 const Main = () =>  {
     //use state   to hold pokemon data
     const [pokeData,setPokedata]=useState()
+    //  loading  pokemon  data
+    const[loading,setLoading]=useState(true)
+    //loading url
+    const[url,setUrl]=useState("https://pokeapi.co/api/v2/pokemon/")
+    
+    const pokeFun=async()=>{
+        setLoading(true)
+        //making API request
+        const res= await axios.get(url);
+        console.log(res)
+    }
+    //initial url
+    useEffect(()=>{
+        pokeFun();
+    },[url]  )
     return (
        <div className="container">
         <div className="left-content">
